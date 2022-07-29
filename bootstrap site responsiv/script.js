b0dfd0135b82517274601cf1de6ae77219beeeb5
kemalkujovic <107282806+kemalkujovic@users.noreply.github.com>
2022-07-30 00:18:03 +0200

    Add files via upload

---
+++ bootstrap site responsiv/script.js
@@ -0,0 +1,11 @@
+// const mobileMenu = () => {
+//     let menu = document.querySelector('.collapse ul');
+//     let btn = document.querySelector('.container button');
+//     if(btn.innerText === 'MENU'){
+//         menu.style.display = 'block';
+//         btn.innerText = 'CLOSE';
+//     } else{
+//         menu.style.display = 'none';
+//         btn.innerText = 'MENU';
+//     }
+// }
