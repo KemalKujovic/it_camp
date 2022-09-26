e382b6feb6236eba66b9da63d8307d32332b7046
kemalkujovic <107282806+kemalkujovic@users.noreply.github.com>
2022-09-26 15:04:02 +0200

    Add files via upload

--- zadaci_1/script.js
+++ zadaci_1/script.js
@@ -2333,6 +2333,20 @@ for (let i = 0; i < noviUser.interesovanja.length; i++) {
 //     .join("");
 // }
 
+// codewars
+// Create phone number
+
+// function createPhoneNumber(num) {
+//   // returns "(123) 456-7890"
+//   let prvaTri = num.filter((el, i) => i < 3);
+//   prvaTri = `(${prvaTri.join("")}) `;
+//   let drugaTri = num.filter((el, i) => i < 6 && i > 2);
+//   drugaTri = `${drugaTri.join("")}-`;
+//   let trecaCetri = num.filter((el, i) => i > 5 && i < num.length).join("");
+
+//   return `${prvaTri}${drugaTri}${trecaCetri}`;
+// }
+
 // console.log(switcheroo("acb"));
 // console.clear();
 
@@ -2826,6 +2840,110 @@ console.clear();
 // }
 // console.log(solution(10));
 
+// codewars
+// Stop gninnipS My sdroW!
+// function spinWords(string) {
+//   string = string.split(" ").map((el, i) => {
+//     if (el.length >= 5) {
+//       return el.split("").reverse().join("");
+//     } else {
+//       return el;
+//     }
+//   });
+
+//   return string.join(" ");
+// }
+// console.log(spinWords("Hey fellow warriors"));
+
+// codewars
+// Create Phone Number
+// function createPhoneNumber(num) {
+//   // returns "(123) 456-7890"
+//   num = num.join("");
+
+//   return `(${num.slice(0, 3)}) ${num.slice(3, 6)}-${num.slice(6, num.length)}`;
+// }
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// codewars
+// Convert string to camel case
+// splice
+// function toCamelCase(str) {
+//   str = str.split("");
+//   return str
+//     .map((el, i) => {
+//       if (el === "-" || el === "_") {
+//         el = str[i + 1].toUpperCase();
+//         str.splice(i + 1, 1);
+//       }
+//       return el;
+//     })
+//     .join("");
+// }
+
+// console.log(toCamelCase("the-stealth-warrior"));
+// console.log(toCamelCase("the_stealth_warrior"));
+
+// codewars
+// Simple Pig Latin
+
+// function pigIt(s) {
+//   return s
+//     .split(" ")
+//     .map((e, i) => {
+//       if (e[0] === "!" || e[0] === "?") {
+//         return e;
+//       } else {
+//         return e.slice(1) + e[0] + "ay";
+//       }
+//     })
+//     .join(" ");
+// }
+
+// console.log(pigIt("Hello !oy World")); // elloHay // oy!ay
+
+// codewars
+// pete, the baker
+function cakes(recipe, available) {
+  let br;
+
+  for (let key in recipe) {
+    if (available[key]) {
+      const noviRecp = Math.floor(available[key] / recipe[key]) || 0;
+
+      if (!br || noviRecp < br) {
+        br = noviRecp;
+      }
+    } else {
+      return 0;
+    }
+  }
+  return br;
+}
+console.log(
+  cakes(
+    { flour: 500, sugar: 200, eggs: 1 },
+    { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+  )
+);
+// console.log(
+//   cakes(
+//     { flour: 500, sugar: 200, eggs: 1 },
+//     { sugar: 500, flour: 2000, milk: 2000 }
+//   )
+// );
+// console.log(
+//   cakes(
+//     { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 },
+//     { sugar: 500, flour: 2000, milk: 2000 }
+//   )
+// );
+// console.log(
+//   cakes(
+//     { flour: 500, sugar: 200, eggs: 1 },
+//     { sugar: 500, flour: 2000, milk: 2000 }
+//   )
+// );
 
 // codewars
 // Find The Parity Outlier
